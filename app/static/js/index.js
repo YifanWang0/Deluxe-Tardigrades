36caba6ddff50fe00688b60278c84788cbb78eec
YifanWang0 <ywang00@stuy.edu>
2020-06-13 17:59:24 -0400

    added dropdown and buttons for graphs

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,57 +1,49 @@
-// // set the dimensions and margins of the graph
-// var margin = {top: 20, right: 20, bottom: 30, left: 40},
-//     width = 960 - margin.left - margin.right,
-//     height = 500 - margin.top - margin.bottom;
-//
-// // set the ranges
-// var x = d3.scaleBand()
-//           .range([0, width])
-//           .padding(0.1);
-// var y = d3.scaleLinear()
-//           .range([height, 0]);
-//
-// // append the svg object to the body of the page
-// // append a 'group' element to 'svg'
-// // moves the 'group' element to the top left margin
-// var svg = d3.select("body").append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform",
-//           "translate(" + margin.left + "," + margin.top + ")");
-//
-// // get the data
-// d3.csv("sales.csv", function(error, data) {
-//   if (error) throw error;
-//
-//   // format the data
-//   data.forEach(function(d) {
-//     d.sales = +d.sales;
-//   });
-//
-//   // Scale the range of the data in the domains
-//   x.domain(data.map(function(d) { return d.salesperson; }));
-//   y.domain([0, d3.max(data, function(d) { return d.sales; })]);
-//
-//   // append the rectangles for the bar chart
-//   svg.selectAll(".bar")
-//       .data(data)
-//     .enter().append("rect")
-//       .attr("class", "bar")
-//       .attr("x", function(d) { return x(d.salesperson); })
-//       .attr("width", x.bandwidth())
-//       .attr("y", function(d) { return y(d.sales); })
-//       .attr("height", function(d) { return height - y(d.sales); });
-//
-//   // add the x Axis
-//   svg.append("g")
-//       .attr("transform", "translate(0," + height + ")")
-//       .call(d3.axisBottom(x));
-//
-//   // add the y Axis
-//   svg.append("g")
-//       .call(d3.axisLeft(y));
-//
-// });
+//changing buttons displayed (for rendering graphs) according to option selected
+var grade_btn = document.getElementById("grade");
+var floor_btn = document.getElementById("floor");
+var location_btn = document.getElementById("location");
+var graphSelection = document.getElementById("graph");
 
-console.log("hello");
+var displayButtons = function(){
+  var value = graphSelection.value;
+  if (value == "lockers-trade" || value == "popularity"){
+    grade_btn.style.display = "none";
+    floor_btn.style.display = "inline";
+    location_btn.style.display = "inline";
+  } else if (value == "users-buddy" || value == "lockers-registered") {
+    grade_btn.style.display = "inline";
+    floor_btn.style.display = "inline";
+    location_btn.style.display = "inline";
+  }
+}
+
+graphSelection.addEventListener("change", displayButtons);
+
+//trying to get a graph to work
+var svgWidth = 500;
+var svgHeight = 300;
+
+var svg = d3.select('svg')
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+var barPadding = 5;
+var barWidth = (svgWidth / dataset.length);
+
+var barChart = svg.selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .attr("y", function(d) {
+    return svgHeight - d
+  })
+  .attr("height", function(d) {
+    return d;
+  })
+  .attr("width", barWidth - barPadding)
+  .attr("transform", function (d, i) {
+    var translate = [barWidth * i, 0];
+    return "translate("+ translate +")";
+  });
